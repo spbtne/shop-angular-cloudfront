2b46624dc8b13285ee837fdf82679df2af9212d6
Nikita Tushin <nikita_tushin@epam.com>
2023-10-09 22:31:37 +0300

    add: task-3 solution

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,17 +7,17 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: false,
   apiEndpoints: {
-    product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    product: 'https://5j0bknz4y2.execute-api.eu-west-1.amazonaws.com',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    bff: 'https://d66do7j31i.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
-    product: false,
+    product: true,
     order: false,
     import: false,
-    bff: false,
+    bff: true,
     cart: false,
   },
 };
