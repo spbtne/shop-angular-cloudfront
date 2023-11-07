9dd31dd9602e8d3a9343fc48526b59d335839933
Nikita Tushin <nikita_tushin@epam.com>
2023-11-08 00:29:37 +0300

    task-7

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://5j0bknz4y2.execute-api.eu-west-1.amazonaws.com',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://ux15ee9p11.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://87mw3206u1.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://d66do7j31i.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
